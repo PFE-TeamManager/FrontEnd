1c96f44f5638b4ee52d7b62c8ef8111249399728
AbdallahOuahib <ouahibabdallah@gmail.com>
2020-05-07 22:40:49 +0000

    Boostrap Base

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Login from './components/Auth/Login';
-//import Accueil from './components/accueil/Accueil';
+
 function App() {
   return (
 
