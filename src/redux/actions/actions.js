ddaffc52311ec82d6c7fbe3c67587afa3e8a4535
AbdallahOuahib <ouahibabdallah@gmail.com>
2020-06-14 14:52:54 +0100

    delete image from redux

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -5,11 +5,6 @@ import {
   PROJECT_ERROR, PROJECT_FORM_UNLOAD,
 
 
-  IMAGE_DELETE_REQUEST, IMAGE_DELETED,
-  IMAGE_UPLOAD_ERROR,IMAGE_UPLOAD_REQUEST,
-  IMAGE_UPLOADED,
-
-
   USER_CONFIRMATION_SUCCESS,USER_LOGIN_SUCCESS,
   USER_LOGOUT,USER_PROFILE_ERROR,USER_PROFILE_RECEIVED,
   USER_PROFILE_REQUEST,USER_REGISTER_COMPLETE,USER_REGISTER_SUCCESS,
@@ -242,61 +237,6 @@ export const userProfileFetch = (userId) => {
   }
 };
 /*****************END User Action****************/
-
-
-
-
-/*****************Image Action****************/
-export const imageUploaded = (data) => {
-  return {
-    type: IMAGE_UPLOADED,
-    image: data
-  }
-};
-
-export const imageUploadRequest = () => {
-  return {
-    type: IMAGE_UPLOAD_REQUEST,
-  }
-};
-
-export const imageUploadError = () => {
-  return {
-    type: IMAGE_UPLOAD_ERROR,
-  }
-};
-
-export const imageUpload = (file) => {
-  return (dispatch) => {
-    dispatch(imageUploadRequest());
-    return requests.upload('/images', file)
-      .then(response => dispatch(imageUploaded(response)))
-      .catch(() => dispatch(imageUploadError))
-  }
-};
-
-export const imageDeleteRequest = () => {
-  return {
-    type: IMAGE_DELETE_REQUEST,
-  }
-};
-
-export const imageDelete = (id) => {
-  return (dispatch) => {
-    dispatch(imageDeleteRequest());
-    return requests.delete(`/images/${id}`)
-      .then(() => dispatch(imageDeleted(id)));
-  }
-};
-
-export const imageDeleted = (id) => {
-  return {
-    type: IMAGE_DELETED,
-    imageId: id
-  }
-};
-/*****************END Image Action****************/
-
 
 
 /*********Comment Action**********/
